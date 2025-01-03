2af10aa665a96c5f08dbc75bb8c444011a953950
awbwtest <7970264@qq.com>
2025-01-03 10:39:54 +0800

    Update custom.sh

--- files/etc/uci-defaults/custom.sh
+++ files/etc/uci-defaults/custom.sh
@@ -1,41 +1,41 @@
 #!/bin/sh
 
-# 设置默认防火墙规则，方便虚拟机首次访问 WebUI
-uci set firewall.@zone[1].input='ACCEPT'
+#设置默认防火墙规则，方便虚拟机首次访问WebUI
+uci 设置防火墙。@zone[1].input = '接受'设置防火墙.@zone[1] .input = 'ACCEPT'
 
-# 设置主机名映射，解决安卓原生 TV 无法联网的问题
-uci add dhcp domain
-uci set "dhcp.@domain[-1].name=time.android.com"
-uci set "dhcp.@domain[-1].ip=203.107.6.88"
+#设置主机名地图，解决安卓电视无法联网的问题
+uci 添加 dhcp 域
+uci 设置“dhcp.@domain[-1] .name =time.android.com” 设置“dhcp.@domain[-1] .name =time.android.com”
+uci 设置“dhcp.@domain[-1] .ip = 203 .107.6.88”设置“dhcp.@domain[-1] .ip = 203 .107.6.88”
 
 
-# 根据网卡数量配置网络
-count=0
-for iface in /sys/class/net/*; do
-  iface_name=$(basename "$iface")
-  # 检查是否为物理网卡（排除回环设备和无线设备）
-  if [ -e "$iface/device" ] && echo "$iface_name" | grep -Eq '^eth|^en'; then
-    count=$((count + 1))
-  fi
-done
+#根据网络退款数量配置
+统计 = 00
+for对于 /sys/class/net/* 中的 iface；做in /sys/class/net/*;做
+  iface_name =$(基本名称“ $iface ”) iface_name =$(基本名称" $iface " )
+  #检查是否为物理端口（排除回环设备和无线设备）#检查是否为物理端口（排除回环设备和无线设备）
+  if [ -e  " $iface /device" ] && echo " $iface_name " | grep -Eq   '^eth|^en' ;然后if [ -e    " $iface /device" ] && echo " $iface_name " | grep -Eq     '^eth|^en' ;然后
+    计数=$((计数+1))计数=$((计数+1))
+  菲菲
+完毕
 
 # 网络设置
-if [ "$count" -eq 1 ]; then
-  uci set network.lan.proto='dhcp'
-elif [ "$count" -gt 1 ]; then
-  uci set network.lan.ipaddr='192.168.100.1'
-fi
+if如果[“ $count ” -eq   1 ];然后“ $count ”    -eq   1 ];然后
+  uci 设置network.lan .proto = 'dhcp'设置network.lan .proto = 'dhcp'
+elifelif [“ $count ”-gt 1 ];然后$count ”   -gt    1 ]；然后
+  uci 设置网络.lan.ipaddr ='192.168.1.1'
+菲
 
 # 设置所有网口可访问网页终端
-uci delete ttyd.@ttyd[0].interface
+uci 删除 ttyd.@ttyd[0].interface
 
-# 设置所有网口可连接 SSH
-uci set dropbear.@dropbear[0].Interface=''
-uci commit
+#设置所有网口可连接SSH
+uci 设置 dropbear.@dropbear[0] .Interface = ''
+uci提交
 
 # 设置编译作者信息
-FILE_PATH="/etc/openwrt_release"
-NEW_DESCRIPTION="Compiled by wukongdaily"
-sed -i "s/DISTRIB_DESCRIPTION='[^']*'/DISTRIB_DESCRIPTION='$NEW_DESCRIPTION'/" "$FILE_PATH"
+FILE_PATH =“/etc/openwrt_release”
+新描述="由悟空日报编译"
+sed -i  “s /DISTRIB_DESCRIPTION = '[^' ]* '/DISTRIB_DESCRIPTION='   $NEW_DESCRIPTION   '/” “ $FILE_PATH ”
 
-exit 0
+出口0
